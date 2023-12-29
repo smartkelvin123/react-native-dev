4cc1efb96b40cc339b0b0b676dd19e8112e29f1b
unknown <smartkelvin66@gmail.com>
2023-12-29 06:51:26 +0100

    dev

---
+++ src/app/(days)/day2/index.tsx
@@ -0,0 +1,18 @@
+import { View, Text, Button } from "react-native";
+import React from "react";
+import { Link, Stack } from "expo-router";
+
+const DayDetailsScreen = () => {
+  return (
+    <View>
+      <Stack.Screen options={{ title: "Day 2: Onboarding" }} />
+      <Text>Day Details Screen</Text>
+
+      <Link href="/(days)/day2/onboarding" asChild>
+        <Button title="Go to onboarding" />
+      </Link>
+    </View>
+  );
+};
+
+export default DayDetailsScreen;
---
+++ src/app/_layout.tsx
@@ -0,0 +1,49 @@
+import { Stack } from "expo-router";
+import { useEffect } from "react";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+
+import {
+  useFonts,
+  Inter_900Black,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_400Regular,
+} from "@expo-google-fonts/inter";
+import {
+  AmaticSC_400Regular,
+  AmaticSC_700Bold,
+} from "@expo-google-fonts/amatic-sc";
+
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  const [fontsLoaded, fontError] = useFonts({
+    Inter: Inter_400Regular,
+    InterSemi: Inter_600SemiBold,
+    InterBold: Inter_700Bold,
+    InterBlack: Inter_900Black,
+
+    Amatic: AmaticSC_400Regular,
+    AmaticBold: AmaticSC_700Bold,
+  });
+
+  useEffect(() => {
+    if (fontsLoaded || fontError) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Stack screenOptions={{}}>
+        <Stack.Screen name="index" options={{ title: "DEVember" }} />
+      </Stack>
+    </GestureHandlerRootView>
+  );
+}
