d6e895aeb207c1b129f1d01aca4281b798470227
unknown <smartkelvin66@gmail.com>
2024-01-07 09:46:50 +0100

    smart

--- src/app/(days)/day5/airbnb.tsx
+++ src/app/(days)/day5/airbnb.tsx
@@ -1,12 +1,38 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React from "react";
+import MapView, { Marker } from "react-native-maps";
+import { Stack } from "expo-router";
+import apartment from "../../../../assets/data/day5/Apartment.json";
 
-const Airbnb = () => {
+const AirbnbScreen = () => {
+  console.log(apartment);
   return (
     <View>
-      <Text>airbnb</Text>
+      <Stack.Screen options={{ headerShown: false }} />
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+        <Marker
+          coordinate={{ latitude: 37.78825, longitude: -122.4324 }}
+          title="Hello"
+          description="World"
+        />
+      </MapView>
     </View>
   );
 };
 
-export default Airbnb;
+const styles = StyleSheet.create({
+  map: {
+    width: "100%",
+    height: "100%",
+  },
+});
+
+export default AirbnbScreen;
