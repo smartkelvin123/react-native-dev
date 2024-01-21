73342949a7dd3b96dbe485d5a6d151db9850836e
unknown <smartkelvin66@gmail.com>
2024-01-21 03:37:48 +0100

    memos

---
+++ src/components/day7/MemoListItem.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const MemoListItem = () => {
+  return (
+    <View>
+      <Text>MemoListItem</Text>
+    </View>
+  );
+};
+
+export default MemoListItem;
