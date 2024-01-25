17cf7e9eb2d492747a51eb52c700aac6742d0753
unknown <smartkelvin66@gmail.com>
2024-01-25 22:00:41 +0100

    added -auth

---
+++ src/app/(days)/day9/index.tsx
@@ -0,0 +1,29 @@
+import { View, Text, Button } from "react-native";
+import React from "react";
+import { Link, Stack } from "expo-router";
+import MarkdownDisplay from "../../../components/MarkdownDisplay";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const description = `
+# Authentication
+AWS Amplify v6 Authentication`;
+
+const DayDetailsScreen = () => {
+  return (
+    <SafeAreaView edges={["bottom"]} style={{ flex: 1 }}>
+      <Stack.Screen options={{ title: "Day 9: Auth" }} />
+
+      <MarkdownDisplay>{description}</MarkdownDisplay>
+
+      <Link
+        // href="/(days)/day9/protected/"
+        href={"/day9/auth/sign-in"}
+        asChild
+      >
+        <Button title="Go to Protected App" />
+      </Link>
+    </SafeAreaView>
+  );
+};
+
+export default DayDetailsScreen;
